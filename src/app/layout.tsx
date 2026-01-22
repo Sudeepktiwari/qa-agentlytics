c9a199e8b8b630df3f5c40f5809ad4cc4fcb1220
Sudeep Tiwari <kt.sudeep@gmail.com>
2026-01-22 15:47:46 +0530

    feat(analytics): add google tag manager and ahrefs analytics scripts
    Add tracking scripts for Google Tag Manager and Ahrefs Analytics to enable website traffic monitoring and SEO analysis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,26 @@ export default function RootLayout({
   return (
     <html lang="en">
       <meta name="msvalidate.01" content="F4758ADAFF276132743477B9EAFC7467" />
+      <script>
+        {`
+    (function(w,d,s,l,i){
+      w[l]=w[l]||[];
+      w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+      var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),
+          dl=l!='dataLayer'?'&l='+l:'';
+      j.async=true;
+      j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+      f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5JBZ4PFL');
+  `}
+      </script>
 
+      <script
+        src="https://analytics.ahrefs.com/analytics.js"
+        data-key="2qN9f9YcTPSBZYmivdCaVw"
+        async
+      ></script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning={true}
